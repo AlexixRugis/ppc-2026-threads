8353e59c774a5ec9330c28edd2023c2c178f8bb6
Alexey Gutyansky <75169888+AlexixRugis@users.noreply.github.com>
2026-04-10 20:52:50 +0300

    Update ops_tbb.cpp

--- tasks/gutyansky_a_img_contrast_incr/tbb/src/ops_tbb.cpp
+++ tasks/gutyansky_a_img_contrast_incr/tbb/src/ops_tbb.cpp
@@ -55,7 +55,7 @@ bool GutyanskyAImgContrastIncrTBB::RunImpl() {
     tbb::parallel_for(tbb::blocked_range<size_t>(0, sz), [&](const auto &range) {
       for (auto idx = range.begin(); idx != range.end(); ++idx) {
         uint16_t old_value = input[idx];
-        uint16_t new_value = (std::numeric_limits<uint8_t>::max(); * (old_value - lower_bound)) / delta;
+        uint16_t new_value = (std::numeric_limits<uint8_t>::max() * (old_value - lower_bound)) / delta;
         output[idx] = static_cast<uint8_t>(new_value);
       }
     });
