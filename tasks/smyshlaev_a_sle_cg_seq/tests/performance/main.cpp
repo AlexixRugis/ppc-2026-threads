1534ff4d6d751caef6cba89af8cc7ef3d9bc478e
P_Alex_Tr <aleksandr.smyshlaev@yandex.ru>
2026-04-20 18:04:58 +0300

    add code

--- tasks/smyshlaev_a_sle_cg_seq/tests/performance/main.cpp
+++ tasks/smyshlaev_a_sle_cg_seq/tests/performance/main.cpp
@@ -9,6 +9,7 @@
 #include "smyshlaev_a_sle_cg_seq/omp/include/ops_omp.hpp"
 #include "smyshlaev_a_sle_cg_seq/seq/include/ops_seq.hpp"
 #include "smyshlaev_a_sle_cg_seq/tbb/include/ops_tbb.hpp"
+#include "smyshlaev_a_sle_cg_seq/stl/include/ops_stl.hpp"
 #include "util/include/perf_test_util.hpp"
 
 namespace smyshlaev_a_sle_cg_seq {
@@ -80,7 +81,7 @@ TEST_P(SmyshlaevASleCgPerfTests, RunPerfModes) {
 namespace {
 
 const auto kAllPerfTasks =
-    ppc::util::MakeAllPerfTasks<InType, SmyshlaevASleCgTaskSEQ, SmyshlaevASleCgTaskOMP, SmyshlaevASleCgTaskTBB>(
+    ppc::util::MakeAllPerfTasks<InType, SmyshlaevASleCgTaskSEQ, SmyshlaevASleCgTaskOMP, SmyshlaevASleCgTaskTBB, SmyshlaevASleCgTaskSTL>(
         PPC_SETTINGS_smyshlaev_a_sle_cg_seq);
 
 const auto kGtestValues = ppc::util::TupleToGTestValues(kAllPerfTasks);
